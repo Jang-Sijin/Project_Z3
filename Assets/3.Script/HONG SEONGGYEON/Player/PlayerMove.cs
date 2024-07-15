b146111b5128532fe47481e3221c1d50eb93085d
홍성견 <ghdtjdrus114@gmail.com>
2024-07-15 21:27:38 +0900

    [update] Monster logics

---
+++ Assets/3.Script/HONG SEONGGYEON/Player/PlayerMove.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    public float speed = 5.0f; // 이동 속도
+
+    void Update()
+    {
+        // 입력 받기
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        // 이동 방향 설정
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        // 이동
+        transform.Translate(movement * speed * Time.deltaTime, Space.World);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("맞음");
+        }
+    }
+}
+
+
