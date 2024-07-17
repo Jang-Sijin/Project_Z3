104fd2894c3bf9f343479a401aeaab1fa8af4a2f
이준형 <dkflfdj@gmail.com>
2024-07-17 21:14:36 +0900

    [Adding] Monster Hit UI Effect, MainCity UI
    몬스터 피격 시 나타나는 글자 효과와 마을에서 사용할 UI를 구현하였습니다.

---
+++ Assets/3.Script/LEE JUNHYEONG/UI/InLobby/MainCityUI.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainCityUI : MonoBehaviour
+{
+    /* <F 상호작용>
+     * 상호작용키 구현 상호작용시 버튼이 빛남
+     * 가까이 가면 버튼이 점프함
+     * 누르면 아무것도 없음
+     * 
+     * <esc>
+     * 누르면 메뉴화면으로 이동
+     * 
+     * <좌클릭>
+     * 달리기 효과 없음
+     */
+}
