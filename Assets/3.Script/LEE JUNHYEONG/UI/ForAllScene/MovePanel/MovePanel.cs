94aa121de98a418b93c68667b6ecaf55fecd8a1c
이준형 <dkflfdj@gmail.com>
2024-07-25 21:27:56 +0900

    [Add] Adding Selecte Charactor BackGroundLoop
    캐릭터 선택 창 배경 이동을 구현하였습니다.

--- Assets/3.Script/LEE JUNHYEONG/UI/ForAllScene/MovePanel/MovePanel.cs
+++ Assets/3.Script/LEE JUNHYEONG/UI/ForAllScene/MovePanel/MovePanel.cs
@@ -4,20 +4,54 @@ using UnityEngine;
 using DG.Tweening;
 using DG.Tweening.Core.Easing;
 
-public class NewBehaviourScript : MonoBehaviour
+public class MovePanel : MonoBehaviour
 {
-    [Header("방향")]
+    [Header("목표 포지션")]
     [SerializeField]
-    private Vector2 direction;
+    private Vector2 targetPos;
+
+    [Header("원본 포지션")]
+    [SerializeField]
+    private Vector2 originPos;
 
     [Header("트위닝 기간")]
     [SerializeField]
     private float duration;
 
+    [Header("딜레이 시간")]
+    [SerializeField]
+    private float delay;
+
     [SerializeField] private Ease ease;
 
-    private void OnEnable()
-    {
+    RectTransform rectTransform;
 
+    Tween tween;
+
+    private void Start()
+    {
+        rectTransform = GetComponent<RectTransform>();
+    }
+
+    public void GoToTargetPos()
+    {
+        if (tween != null)
+        {
+            tween.Kill();
+        }
+
+
+        tween = rectTransform.DOAnchorPos(targetPos, duration).SetEase(ease).SetDelay(delay);
+    }
+
+
+    public void GoToOriginPos()
+    {
+        if (tween != null)
+        {
+            tween.Kill();
+        }
+    
+        tween = rectTransform.DOAnchorPos(originPos, duration).SetEase(ease);
     }
 }
