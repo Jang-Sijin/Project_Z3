f2e3d4d972a538debad434a80890f240d1fac15a
김지훈 <64973117+RiconQ@users.noreply.github.com>
2024-07-26 18:00:11 +0900

    [Update] IngamePauseUI Setup

---
+++ Assets/3.Script/KIM JIHUN/Build/UI/Build_IngamePauseUI.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Build_IngamePauseUI : MonoBehaviour
+{
+    public void CloseIngamePauseUI()
+    {
+
+    }
+}
